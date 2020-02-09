ce4420aecff634907772ee0768124d9a758f73fe
RitashaVerma <vermaritasha@yahoo.co.in>
2020-02-09 17:38:19 +0530

    Success

--- First_project/JOB_PORTAL/views.py
+++ First_project/JOB_PORTAL/views.py
@@ -6,6 +6,6 @@ def home(request):
     return HttpResponse('<h1>Well, Hello !</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About !</h1>')
+    return HttpResponse('<h1>About page!</h1>')
 
 # Create your views here.
